6e3241ee5f05bfbb0db5c7636a4e7e531837b712
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NewProject;
+using System.Linq;
+using System.Collections.Generic;
+using DevExpress.DashboardCommon;
+using DevExpress.Web;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void ASPxDashboardViewer1_DataLoading(object sender, DevExpress.DashboardWeb.DataLoadingWebEventArgs e)
+        {
+            List<MyClass> list = CreateDataSource();
+            e.Data = list;
+        }
+
+        private static List<MyClass> CreateDataSource()
+        {
+            List<MyClass> list = new List<MyClass>(10);
+            Random _r = new Random();
+            for (int i = 0; i < 10; i++)
+                list.Add(new MyClass() { ID = i, Name = "Name" + i, Sales = (float)_r.NextDouble(), Date = DateTime.Now.AddDays(i) });
+            return list;
+        }
+    }
+}
